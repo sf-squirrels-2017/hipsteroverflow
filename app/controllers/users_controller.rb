2930e75894fe49f718434e1358cd13b0ce9ead83
Lisa Nguyen <lisathuanguyen@gmail.com>
2017-02-02 15:03:51 -0800

    registering a user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    login(@user)
+    redirect "users/#{@user.username}"
+  else
+    erb :'users/new'
+  end
+end
