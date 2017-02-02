8a144083e0ce2619a875f88df0c4f429e0f4bc83
Lisa Nguyen <lisathuanguyen@gmail.com>
2017-02-02 15:19:49 -0800

    a user can register

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,29 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
   has_many :comments
-  has_many :votes 
+  has_many :votes
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @raw_password = hashed_password
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def validate_password
+    errors.add(:password, "is required") if @raw_password.nil?
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.hashed_password == password
+      @user
+    else
+      nil
+    end
+  end
 
 end
