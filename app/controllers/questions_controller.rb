9cd69227d8dc24aa7c673c5d22db8a18d00e189d
Jon <jon.bent@me.com>
2017-02-02 14:36:08 -0800

    adds index for questions, and adds controller for question

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,43 @@
+get '/questions' do
+  @questions = Question.all
+  erb :'questions/index'
+end
+
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect '/questions'
+  else
+    erb :'questions/new'
+  end
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  erb :'questions/show'
+end
+
+get '/questions/:id/edit' do
+  @question = Question.find(params[:id])
+  erb :'questions/edit'
+end
+
+put '/questions/:id' do
+  @question = Question.find(params[:id])
+  @question.assign_attributes(params[:question])
+  if @question.save
+    redirect '/questions'
+  else
+    erb :'questions/edit'
+  end
+end
+
+delete '/questions/:id' do
+  @question = Question.find(params[:id])
+  @question.destroy
+  redirect '/questions'
+end
